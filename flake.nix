765a9c8c6b99d8e54cb6e360297326830a40fee5
Daniel Sandbecker <daniel.sandbecker@hb.se>
2024-03-08 20:37:04 +0100

    Initialize new project

---
+++ flake.nix
@@ -0,0 +1,20 @@
+{
+  description = "A basic flake with a shell";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-23.11-darwin";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs = { nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          packages = [
+            pkgs.bashInteractive
+            pkgs.esbuild
+            pkgs.nodePackages.prettier
+          ];
+        };
+      });
+}
