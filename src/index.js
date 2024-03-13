4d9d4f6b867f779cf846e1d6bfea7e5a91ff9e63
Daniel Sandbecker <daniel.sandbecker@hb.se>
2024-03-13 08:35:56 +0100

    Use plain config for default bookmarklet

--- src/index.js
+++ src/index.js
@@ -1,21 +1,11 @@
 import * as config from "../config.json";
 import { createForm } from "./form.js";
 
-function override(config, overrides) {
-  return {
-    ...config,
-    options: { ...config.options, ...overrides.options },
-    defaults: { ...config.defaults, ...overrides.defaults },
-  };
-}
-
-myForm = createForm(
-  override(config, config.overrides.some_vendor),
-  (str, form) => {
-    if (window.prompt(str, str)) {
-      document.body.removeChild(form);
-    }
-  },
-);
+myForm = createForm(config, (str, form) => {
+  const label = (config.labels || {}).copyPrompt || "Copy the generated code";
+  if (window.prompt(label, str)) {
+    form.remove();
+  }
+});
 
 document.body.insertBefore(myForm, document.body.firstChild);
